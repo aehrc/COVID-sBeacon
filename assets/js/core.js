1885168543024d08c269aadd7e827cfaf9000ed4
Yatish Jain <yatish.jain@csiro.au>
2020-04-17 18:45:05 +1000

    attaching API endpoints

--- assets/js/core.js
+++ assets/js/core.js
@@ -22,19 +22,29 @@ covidBeacon.controller('beacon', function($scope, $http) {
     $scope.ref = "";
     $scope.alt = "";
     $scope.isVisible = false;
+    $scope.hits = "";
+    $scope.warning ="";
 
     $scope.ShowHide = function(){
           console.log("clicker");
             $scope.isVisible = !$scope.isVisible;
     }
     $scope.query = function(){
-      //do validation and throw error.
-      var data = {"assemblyId": "hCoV-19","referenceName": "1","referenceBases":$scope.ref,"alternateBases":$scope.alt, "start":$scope.sPos,"end":$scope.ePos};
+      //do validation and throw error. Need to change assembly to  hCoV-19 later.
+      var data = {"assemblyId": "hCoV-19","referenceName": "1","includeDatasetResponses":"HIT","referenceBases":$scope.ref,"alternateBases":$scope.alt, "start":$scope.sPos,"end":$scope.ePos};
       var url = "https://dsug72ummg.execute-api.ap-southeast-2.amazonaws.com/prod/query";
 
       $http({method: 'GET', url: url,params:data}).then(function successCallback(resp) {
-        $scope.hits = resp.data;
-        console.log($scope.hits);
+        var hits = resp.data;
+        console.log(hits);
+        if( hits.exists == true){
+          $scope.hits = hits.datasetAlleleResponses;
+          console.log($scope.hits);
+        }else{
+          $scope.warning = "No hits matching the search criteria";
+          console.log($scope.warning );
+        }
+
 
       }, function errorCallback(resp) {
           console.log("Error: " + JSON.stringify(resp));
