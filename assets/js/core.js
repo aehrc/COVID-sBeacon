4bb388f3739cbe2d06c02f5aa320cbb3148c3619
Yatish Jain <yatish.jain@csiro.au>
2020-04-21 11:23:57 +1000

    form validation and other endpoints

--- assets/js/core.js
+++ assets/js/core.js
@@ -17,13 +17,14 @@ var covidBeacon = angular.module('covidBeacon', ['ngMaterial','ngRoute'])
 
 covidBeacon.controller('beacon', function($scope, $http) {
 
-    $scope.sPos ;
-    $scope.ePos ;
-    $scope.ref = "";
-    $scope.alt = "";
+    $scope.sPos;
+    $scope.ePos;
+    $scope.VarType;
+    $scope.ref = $scope.alt ="";
     $scope.isVisible = false;
-    $scope.hits = "";
-    $scope.warning ="";
+    $scope.hits = $scope.warning = $scope.sMin = $scope.sMax = $scope.eMin = $scope.eMax = null;
+    var queryData = "";
+
 
     $scope.ShowHide = function(){
           console.log("clicker");
@@ -31,16 +32,27 @@ covidBeacon.controller('beacon', function($scope, $http) {
     }
     $scope.query = function(){
       //do validation and throw error. Need to change assembly to  hCoV-19 later.
-      var data = {"assemblyId": "hCoV-19","referenceName": "1","includeDatasetResponses":"HIT","referenceBases":$scope.ref,"alternateBases":$scope.alt, "start":$scope.sPos,"end":$scope.ePos};
+      if( $scope.sMin != null || $scope.sMax != null || $scope.eMin != null || $scope.eMax != null){
+        $scope.sPos = null;
+        $scope.ePos = null;
+        queryData = {"assemblyId": "hCoV-19","referenceName": "1","includeDatasetResponses":"HIT","referenceBases":$scope.ref,"alternateBases":$scope.alt, "startMin":$scope.sMin,"startMax":$scope.sMax,"endMin":$scope.eMin,"endMax":$scope.eMax,"variantType":$scope.VarType};
+      }else{
+        var queryData = {"assemblyId": "hCoV-19","referenceName": "1","includeDatasetResponses":"HIT","referenceBases":$scope.ref,"alternateBases":$scope.alt, "start":$scope.sPos,"end":$scope.ePos,"variantType":$scope.VarType};
+      }
       var url = "https://dsug72ummg.execute-api.ap-southeast-2.amazonaws.com/prod/query";
-
-      $http({method: 'GET', url: url,params:data}).then(function successCallback(resp) {
+      console.log(queryData);
+      $http({method: 'GET', url: url,params:queryData}).then(function successCallback(resp) {
         var hits = resp.data;
         console.log(hits);
         if( hits.exists == true){
           $scope.hits = hits.datasetAlleleResponses;
+          $scope.warning =null;
           console.log($scope.hits);
+        }else if(hits.error == true){
+          $scope.warning = hits.error.errorMessage;
+          $scope.hits = null;
         }else{
+          $scope.hits = null;
           $scope.warning = "No hits matching the search criteria";
           console.log($scope.warning );
         }
