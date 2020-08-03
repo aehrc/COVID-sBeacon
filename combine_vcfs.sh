9b2f45baafeb46ec11ab2d66b807ca08fd711907
Brendan Hosking <brendan.hosking@csiro.au>
2020-08-03 11:59:48 +1000

    REFACTOR: Combine VCFs from multiple directories

--- combine_vcfs.sh
+++ combine_vcfs.sh
@@ -2,8 +2,8 @@
 set -e
 
 REFERENCE_FILE=$1
-VCF_DIRECTORY=$2
-OUTPUT_PREFIX=$3
+OUTPUT_PREFIX=$2
+VCF_DIRECTORIES=( "${@:3}" )
 
 TMP_DIRECTORY="/tmp/${OUTPUT_PREFIX}"
 MERGE_FILE="${TMP_DIRECTORY}/merge_files"
@@ -13,14 +13,17 @@ if [[ ${REFERENCE_FILE:0:1} != "/" ]]; then
 fi
 
 mkdir -p "${TMP_DIRECTORY}"
-cd "${VCF_DIRECTORY}"
 echo "" > "${MERGE_FILE}"
-for vcf in *.vcf; do
-  bcftools norm --output-type z --check-ref s --fasta-ref "${REFERENCE_FILE}" --no-version "${vcf}" > "${TMP_DIRECTORY}/${vcf}.gz"
-  bcftools index "${TMP_DIRECTORY}/${vcf}.gz"
-  echo "${TMP_DIRECTORY}/${vcf}.gz" >> "${MERGE_FILE}"
+for dir in "${VCF_DIRECTORIES[@]}"; do
+  cd "${dir}"
+  for vcf in *.vcf; do
+    bcftools norm --output-type z --check-ref s --fasta-ref "${REFERENCE_FILE}" --no-version "${vcf}" > "${TMP_DIRECTORY}/${vcf}.gz"
+    bcftools index "${TMP_DIRECTORY}/${vcf}.gz"
+    echo "${TMP_DIRECTORY}/${vcf}.gz" >> "${MERGE_FILE}"
+  done
+  cd "${CALL_DIRECTORY}"
 done
 
-cd "${CALL_DIRECTORY}"
+
 bcftools merge --force-samples --missing-to-ref --file-list "${MERGE_FILE}" --no-version --output-type z --threads 4 > "${OUTPUT_PREFIX}.vcf.gz"
 bcftools index "${OUTPUT_PREFIX}.vcf.gz"
