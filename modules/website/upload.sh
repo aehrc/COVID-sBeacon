f0885457bef2252bf978f2bd9bcfbd6030ac5690
Brendan Hosking <brendan.hosking@csiro.au>
2021-04-22 11:54:11 +1000

    REFACTOR: Remove unused upload.sh arguments

--- modules/website/upload.sh
+++ modules/website/upload.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 set -e
 cd beaconApp
-echo -e "{\n \"apiBaseUrl\" : \"${3}\",\n \"login\" : ${4} \n}" > src/assets/config.json
+echo -e "{\n \"apiBaseUrl\" : \"${2}\",\n \"login\" : ${3} \n}" > src/assets/config.json
 mkdir -p src/environments
-echo -e "export const environment = {\n production: true, \n cloudfront_url: \"${6}\" \n };" >src/environments/environment.prod.ts
-echo -e "export const environment = {\n production: true, \n cloudfront_url: \"${6}\" \n };" >src/environments/environment.ts
+echo -e "export const environment = {\n production: true, \n cloudfront_url: \"${4}\" \n };" >src/environments/environment.prod.ts
+echo -e "export const environment = {\n production: true, \n cloudfront_url: \"${4}\" \n };" >src/environments/environment.ts
 npm ci
-#if [ ${5} = true ]
-#then
 node_modules/@angular/cli/bin/ng build --prod --subresourceIntegrity=true
-#else
-#  node_modules/@angular/cli/bin/ng build --subresourceIntegrity=true
-#fi
 cd dist/beaconApp
 aws s3 sync . "s3://${1}" --delete
-#aws cloudfront create-invalidation --distribution-id ${2} --paths "/index.html"
