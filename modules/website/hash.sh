736b011678dfffb17911b040df5dcef3e0b12b2f
Brendan Hosking <brendan.hosking@csiro.au>
2021-04-22 12:29:53 +1000

    REFACTOR: Clean linting recommendations in hash.sh

--- modules/website/hash.sh
+++ modules/website/hash.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-read -t 0.1 -n 10000 discard # drop whatever stdin gives us; we do not use it
+# shellcheck disable=SC2034
+read -r -t 0.1 -n 10000 discard # drop whatever stdin gives us; we do not use it
+set -e
 cd beaconApp
-HASH=$(find * -type f -not -path "dist/*" -not -path "node_modules/*" -not -path "src/environments/environment.prod.ts" -print0 | sort -z | xargs -0 sha1sum | sha1sum | cut -f1 -d ' ')
-echo {\"hash\": \"${HASH}\"}
+HASH=$(find ./* -type f -not -path "./dist/*" -not -path "./node_modules/*" -not -path "./src/environments/environment.prod.ts" -print0 | sort -z | xargs -0 sha1sum | sha1sum | cut -f1 -d ' ')
+echo "{\"hash\": \"$HASH\"}"
