8991708d17a58d5796f5d97ac1a986d5c0a2658f
Brendan Hosking <brendan.hosking@gmail.com>
2022-06-08 16:28:11 +1000

    FIX: Increase timeout for summariseDataset
    summariseDataset is timing out between checking the etag of every file
    and calling summariseVcf for every file.
    
    Increase the timeout to two minutes, which should be enough to handle up
    to 20 million samples (2048 files). If there are still problems, calling
    submit with a subset of the files first will allow summariseVcf to be
    called in batches (or just increase the timeout again).

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -67,7 +67,7 @@ module "lambda-summariseDataset" {
   handler = "lambda_function.lambda_handler"
   runtime = "python3.6"
   memory_size = 2048
-  timeout = 60
+  timeout = 120
   policy = {
     json = data.aws_iam_policy_document.lambda-summariseDataset.json
   }
